e61e753a2aa1c4ff9dc67d04b532e9fa11592a38
LuvU3OOO <WHT_2022@163.com>
2023-12-21 17:23:43 +0800

    基础框架完成

--- ai-frontend/src/App.vue
+++ ai-frontend/src/App.vue
@@ -3,7 +3,17 @@
 
 </template>
 <script setup>
+import { useDark, useToggle } from '@vueuse/core';
 
+useDark({
+  selector:'html',
+  arrtribute:'class',
+  valueDark:'dark',
+  valueLight:'light'
+})
+useDark({
+  onChanged(dark) {useToggle(dark)}
+})
 </script>
 
 <style scoped>
